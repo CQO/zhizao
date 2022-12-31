38ff44ac4b136d0f857175efed367916dbe8af29
PUGE <mail@puge.org>
2022-12-31 23:20:30 +1100

    save

--- index copy 4.py
+++ index copy 4.py
@@ -24,20 +24,29 @@ try:
     print(strLen)
     for ind1 in range(strLen):
         print('进度%s/%s' % (ind1, strLen))
+        strArr[ind1] = strArr[ind1].replace(', ', ' ')
+        strArr[ind1] = strArr[ind1].replace('"', '')
         tempArr = strArr[ind1].split(',')
         if (tempArr and tempArr[0] and tempArr[1] != 0):
             tempArr[0] = tempArr[0].replace('\ufeff', '')
             idList.append(int(tempArr[0]))
-            outPut.append([int(tempArr[0]), tempArr[1], tempArr[2], tempArr[3], tempArr[4], tempArr[5]])
+            temp2 = []
+            for item2 in tempArr[5:]:
+                if (item2):
+                    temp2.append(item2)
+            outPut.append([int(tempArr[0]), tempArr[1], tempArr[2], tempArr[3], tempArr[4], temp2])
+        else:
+            break
     result={}
     for i in range(len(idList)):
         res=[]
         print(idList[i])
         for j in range(i+1,len(idList)):
             x=pearsonr(datas[idList[i]],datas[idList[j]])[0]
-            if (str(idList[i]) not in result):
-                result[str(idList[i])] = []
-            result[str(idList[i])].append([idList[j], round(x, 2)])
+            if (x > 0.7):
+                if (str(idList[i]) not in result):
+                    result[str(idList[i])] = []
+                result[str(idList[i])].append([idList[j], round(x, 2)])
     exfile = open('output4.txt', 'w', encoding = 'UTF-8')
     exfile.write(json.dumps({
         "data": outPut,
